8356956159808b99424139e5e308093418a85c24
Oussema Dammak <oussemadammak01@gmail.com>
2025-09-28 20:49:50 +0100

    add property list (static)

---
+++ app/components/properties/propertyList.tsx
@@ -0,0 +1,14 @@
+import PropertyListItem from "./propertyListItem";
+
+const PropertyList = () => {
+    return(
+        <>
+        <PropertyListItem/>
+        <PropertyListItem/>
+        <PropertyListItem/>
+        <PropertyListItem/>
+        <PropertyListItem/>
+        </>
+    )
+}
+export default PropertyList;
