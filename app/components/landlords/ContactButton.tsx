bd6f0a2713958bae8c5ac85ab07a2ca6120d81f7
Oussema Dammak <oussemadammak01@gmail.com>
2025-09-28 21:52:15 +0100

    landlord details page (static)

---
+++ app/components/landlords/ContactButton.tsx
@@ -0,0 +1,9 @@
+const ContactButton=()=>{
+    return (
+        <div className="cursor-pointer hover:bg-opalyachts-dark transition mt-6 py-4 px-6 bg-opalyachts text-white rounded-xl">
+            Contact
+        </div>
+    )
+
+}
+export default ContactButton;
